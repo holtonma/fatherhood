ea50d3218c2fc1275697aee63091afe1db68d52d
holtonma <holtonma@gmail.com>
2011-01-05 17:30:11 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fatherhood::Application.config.secret_token = '7485ed2aac6da00bbfb32f8f79d2042953b08016aba3f936b7f2157283a9bd15fae46e6fa02a5181027f53e7b1009ec9f5f9ce91a80d5702e5ae43f39feb5b2f'
